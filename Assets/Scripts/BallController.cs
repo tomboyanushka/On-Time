fc55ad876d74b453ef067231382e70ffae7b8a86
tomboyanushka <32500051+tomboyanushka@users.noreply.github.com>
2018-05-11 13:41:49 -0400

    Physics, materials, lighting left.

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float speed = 4.0f;
+    Rigidbody rigidbody;
+	// Use this for initialization
+	void Start ()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("W");
+            rigidbody.AddForce(Vector3.forward * speed, ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidbody.AddForce(Vector3.left * speed, ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rigidbody.AddForce(Vector3.back * speed, ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigidbody.AddForce(Vector3.right * speed, ForceMode.Impulse);
+        }
+
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "cube1")
+            Destroy(collision.gameObject);
+        //else if (collision.gameObject.tag == "cube2")
+         
+    }
+}

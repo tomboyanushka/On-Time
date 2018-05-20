ccd0b0507fe818828c77ad7b2a1071fe5e6c6b3a
tomboyanushka <32500051+tomboyanushka@users.noreply.github.com>
2018-05-19 23:28:37 -0400

    New controller, camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,24 +6,41 @@ public class CameraController : MonoBehaviour
 {
     public Transform target;
     Camera cam;
-    float offset = 5.0f;
-    public float smooth = 2.0f;
+    public Vector3 offset = new Vector3(0,5,-8);
+    public float XTilt = 10;
+    public float smooth = 0.09f;
 
-    public Vector2 mouseLook;
-    public Vector2 rotate;
+    Vector3 destination = Vector3.zero;
+    CharacterController charController;
+    float rotateVelocity = 0;
 
-    public Quaternion rotateY;
+   
 
 
     void Start()
     {
-        
+        charController = target.GetComponent<CharacterController>();
     }
 
     void LateUpdate()
     {
-       
 
-        //transform.LookAt( target + offset);
+        MoveToTarget();
+        LookAtTarget();
+       
     }
+
+
+    void MoveToTarget()
+    {
+        destination = charController.PlayerRotation * offset;
+        destination += target.position;
+        transform.position = destination;
+    }
+    void LookAtTarget()
+    {
+        float eulerAngleY = Mathf.SmoothDampAngle(transform.eulerAngles.y, target.eulerAngles.y, ref rotateVelocity, smooth);
+        transform.rotation = Quaternion.Euler(transform.eulerAngles.x, eulerAngleY, 0);
+    }
+
 }
---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    public float inputDelay = 0.1f;
+    public float velocity = 12;
+    public float rotateVelocity = 100;
+
+    public float health = 100f;
+
+    Quaternion playerRotation;
+
+    Rigidbody rigidbody;
+    float forwardInput, rotateInput;
+
+    public Quaternion PlayerRotation
+    {
+        get { return playerRotation; }
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerRotation = transform.rotation;
+        if (GetComponent<Rigidbody>())
+        {
+            rigidbody = GetComponent<Rigidbody>();
+
+        }
+        else Debug.LogError("Character needs a rigidbody");
+	}
+
+    void getInput()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        rotateInput = Input.GetAxis("Horizontal");
+    }
+
+    private void Update()
+    {
+        getInput();
+        Rotate();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (Mathf.Abs(forwardInput) > inputDelay)
+        {
+            rigidbody.velocity = transform.forward * forwardInput * velocity;
+        }
+        else
+            rigidbody.velocity = Vector3.zero;
+    }
+
+    private void Rotate()
+    {
+        if (Mathf.Abs(rotateInput) > inputDelay)
+        {
+            playerRotation *= Quaternion.AngleAxis(rotateVelocity * rotateInput * Time.deltaTime, Vector3.up);
+            transform.rotation = playerRotation;
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "minute")
+        {
+            health -= 10f;
+            Debug.Log(health);
+        }
+        else if (collision.gameObject.tag == "hour")
+        {
+            health -= 20f;
+            Debug.Log(health);
+
+        }
+        else if (collision.gameObject.tag == "Stand")
+        {
+            health = 100f;
+            Debug.Log(health);
+
+        }
+    }
+
+}

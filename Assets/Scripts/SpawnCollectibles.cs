83e86e6982fcb2e87f5a845690db92997d9718ba
tomboyanushka <32500051+tomboyanushka@users.noreply.github.com>
2018-05-21 22:05:48 -0400

    starting animations, lighting

--- Assets/Scripts/SpawnCollectibles.cs
+++ Assets/Scripts/SpawnCollectibles.cs
@@ -7,23 +7,27 @@ public class SpawnCollectibles : MonoBehaviour
     public GameObject[] collectibles;
     Vector3 area;
     float timeDelay;
+    float timeAfterSpawn;
+    bool isSpawned = false;
     
 
 	// Use this for initialization
 	void Start ()
     {
-        
+        timeDelay = timeAfterSpawn = 0f;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
         timeDelay += Time.deltaTime;
-        if (timeDelay >= 5)
+        if (timeDelay >= 3)
         {
             area = new Vector3(Random.Range(60, -60), 5f, Random.Range(70, -70));
             Instantiate(collectibles[Random.Range(0, collectibles.Length)], area, Quaternion.identity);
             timeDelay = 0f;
+            timeAfterSpawn += Time.deltaTime;
         }
+       
 	}
 }
